ecdaf9b3331cd5fd345875fff209be9b0233fae5
Daniel Farrell <danielfarrell76@gmail.com>
2014-07-11 23:12:43 -0400

    Super basic docs on the methods

--- lib/sass.ex
+++ lib/sass.ex
@@ -1,5 +1,11 @@
 defmodule Sass do
+  @moduledoc """
+    Compiles SASS into CSS
+  """
 
+  @doc """
+    Compiles a string of SASS into a string of CSS
+  """
   def compile(string) do
     Sass.Compiler.compile(string)
   end
--- lib/sass/compiler.ex
+++ lib/sass/compiler.ex
@@ -1,11 +1,21 @@
 defmodule Sass.Compiler do
+  @moduledoc """
+    Connection to the NIF for libsass
+  """
+
   @on_load { :init, 0 }
 
+  @doc """
+    Loads the sass.so library
+  """
   def init do
     path = :filename.join(:code.priv_dir(:sass), 'sass')
     :ok = :erlang.load_nif(path, 0)
   end
 
+  @doc """
+    A noop that gets overwritten by the NIF
+  """
   def compile(_) do
     exit(:nif_library_not_loaded)
   end
