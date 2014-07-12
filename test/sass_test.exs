73aae5762e90e3bbc7622d3f2a6570a6548c2a73
Daniel Farrell <danielfarrell76@gmail.com>
2014-07-12 10:00:14 -0400

    Fix test

--- test/sass_test.exs
+++ test/sass_test.exs
@@ -2,7 +2,7 @@ defmodule SassTest do
   use ExUnit.Case
 
   test "Sass.compile/1 compiles a SCSS string to CSS" do
-    scss_string = "/* example.scss */#navbar {width: 80%;height: 23px;ul { list-style-type: none; } li {float: left; a { font-weight: bold; } } }"
+    scss_string = "/* sample_scss.scss */#navbar {width: 80%;height: 23px;ul { list-style-type: none; }; li {float: left; a { font-weight: bold; } } }"
     { :ok, expected_css } = File.read("test/sample_scss.css")
     { :ok, result_css }   = Sass.compile(scss_string)
 
